84b02a1652aead6ba16767f6725b082b314e1366
Dell <nitinprogramer@gmail.com>
2022-04-18 18:28:59 +0530

    added controller

--- src/main/java/com/example/demo/SpringBootDockerUsingJenkisApplication.java
+++ src/main/java/com/example/demo/SpringBootDockerUsingJenkisApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerUsingJenkisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerUsingJenkisApplication.class, args);
 	}
 
+	@RequestMapping("/spring-cicd-pipeline")
+	public String hello() {
+		return "This's the example of spring-boot docker image auto using ci/cd pipeline";
+	}
 }
